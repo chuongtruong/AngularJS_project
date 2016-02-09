d653aa106f43946385f0b09a06cfe09e114ee5e7
ChuongTruong <chuongtruong290893@gmail.com>
2016-02-09 16:32:30 +0200

    frequency

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+var app=angular.module("myApp", ["ui.router"]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+$urlRouterProvider.otherwise("/home");
+$stateProvider
+    .state('app', {
+        abstract: true,
+        templateUrl: 'navbar.html'
+    })
+    .state('home', {
+        parent:'app',
+        url:"/home",
+        templateUrl: 'home.html'
+    })
+    .state('video', {
+        parent: 'app',
+        url:"/video",
+        templateUrl: 'video.html'
+    });
+});
