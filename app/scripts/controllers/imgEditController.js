9f8fc513523d9a71f7cd33ad2451224a036fb4b2
HyeSoo <flyjwayur@gmail.com>
2016-03-02 23:16:18 +0200

    trying to use dataURItoBlob, not working yet

--- app/scripts/controllers/imgEditController.js
+++ app/scripts/controllers/imgEditController.js
@@ -22,6 +22,48 @@ angular.module('myApp')
             $scope.image.onload = $scope.resetImage;
         };
 
+       $scope.sendImage = function () {
+            var fd = new FormData(document.getElementById('fileForm'));
+            fd.append('user', localStorage.getItem("userID"));
+            //console.log(localStorage.getItem("userID")); ==> upload without login, sending a message
+            fd.append('type', $scope.type);
+            //console.log($scope.type);
+            fd.append('mime-type', $scope.mimeType);
+            //console.log($scope.mimeType);
+           console.log($scope.dataURItoBlob($scope.saveImage()));
+
+            var request = AjaxFactory.uploadFile(fd);
+            request.then(function (response) {
+                alert("file Uploaded");
+                $state.go('photo');
+
+            }, function (error) {
+                console.log(error.data);
+                alert(error.data);
+            });
+        };
+    
+        $scope.dataURItoBlob = function (dataURI) {
+            // convert base64/URLEncoded data component to raw binary data held in a string
+            var byteString;
+            if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+                byteString = atob(dataURI.split(',')[1]);
+            } else {
+                byteString = decodeURI(dataURI.split(',')[1]);
+            }
+            // separate out the mime component
+            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+            // write the bytes of the string to a typed array
+            var ia = new Uint8Array(byteString.length);
+            for (var i = 0; i < byteString.length; i++) {
+                ia[i] = byteString.charCodeAt(i);
+            }
+
+            return new Blob([ia], {
+                type: mimeString
+            });
+        };
 
         $scope.init = function () {
             //initialize default values for variables
--- app/scripts/controllers/uploadController.js
+++ app/scripts/controllers/uploadController.js
@@ -155,28 +155,51 @@ angular.module('myApp')
             }
         };
 
-//
-//        $scope.dataURItoBlob = function (dataURI) {
-//            // convert base64/URLEncoded data component to raw binary data held in a string
-//            var byteString;
-//            if (dataURI.split(',')[0].indexOf('base64') >= 0) {
-//                byteString = atob(dataURI.split(',')[1]);
-//            } else {
-//                byteString = decodeURI(dataURI.split(',')[1]);
-//            }
-//            // separate out the mime component
-//            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-//
-//            // write the bytes of the string to a typed array
-//            var ia = new Uint8Array(byteString.length);
-//            for (var i = 0; i < byteString.length; i++) {
-//                ia[i] = byteString.charCodeAt(i);
-//            }
-//
-//            return new Blob([ia], {
-//                type: mimeString
-//            });
-//        };
+        //
+        //        $scope.dataURItoBlob = function (dataURI) {
+        //            // convert base64/URLEncoded data component to raw binary data held in a string
+        //            var byteString;
+        //            if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+        //                byteString = atob(dataURI.split(',')[1]);
+        //            } else {
+        //                byteString = decodeURI(dataURI.split(',')[1]);
+        //            }
+        //            // separate out the mime component
+        //            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+        //
+        //            // write the bytes of the string to a typed array
+        //            var ia = new Uint8Array(byteString.length);
+        //            for (var i = 0; i < byteString.length; i++) {
+        //                ia[i] = byteString.charCodeAt(i);
+        //            }
+        //
+        //            return new Blob([ia], {
+        //                type: mimeString
+        //            });
+        //        };
+
+
+        $scope.dataURItoBlob = function (dataURI) {
+            // convert base64/URLEncoded data component to raw binary data held in a string
+            var byteString;
+            if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+                byteString = atob(dataURI.split(',')[1]);
+            } else {
+                byteString = decodeURI(dataURI.split(',')[1]);
+            }
+            // separate out the mime component
+            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+            // write the bytes of the string to a typed array
+            var ia = new Uint8Array(byteString.length);
+            for (var i = 0; i < byteString.length; i++) {
+                ia[i] = byteString.charCodeAt(i);
+            }
+
+            return new Blob([ia], {
+                type: mimeString
+            });
+        };
 
         //Sending an image info
         $scope.sendImage = function () {
@@ -187,6 +210,30 @@ angular.module('myApp')
             //console.log($scope.type);
             fd.append('mime-type', $scope.mimeType);
             //console.log($scope.mimeType);
+
+            //console.log($scope.dataURItoBlob($scope.saveImage()));
+
+            $scope.dataURItoBlob = function (dataURI) {
+                // convert base64/URLEncoded data component to raw binary data held in a string
+                var byteString;
+                if (dataURI.split(',')[0].indexOf('base64') >= 0) {
+                    byteString = atob(dataURI.split(',')[1]);
+                } else {
+                    byteString = decodeURI(dataURI.split(',')[1]);
+                }
+                // separate out the mime component
+                var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+                // write the bytes of the string to a typed array
+                var ia = new Uint8Array(byteString.length);
+                for (var i = 0; i < byteString.length; i++) {
+                    ia[i] = byteString.charCodeAt(i);
+                }
+
+                return new Blob([ia], {
+                    type: mimeString
+                });
+            };
 
             var request = AjaxFactory.uploadFile(fd);
             request.then(function (response) {
